1d0009c63918ae64e678752ba7ae7faca47da0f4
Gerry Agbobada <git@gagbo.net>
2022-02-08 00:53:00 +0100

    [sofle] Apply bspc/bsls/mins rotation

--- keyboards/sofle/keymaps/gagbo/keymap.c
+++ keyboards/sofle/keymaps/gagbo/keymap.c
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,         KC_2,          KC_3,          KC_4,          KC_5,                        KC_6,     KC_7,          KC_8,          KC_9,          KC_0,       KC_MINS,
-  KC_TAB,   KC_Q,         KC_W,          KC_F,          KC_P,          KC_G,                        KC_J,     KC_L,          KC_U,          KC_Y,          KC_SCLN,    KC_BSLS,
-  KC_LCTL,  HOME_CMK_A,   HOME_CMK_R,    HOME_CMK_S,    HOME_CMK_T,    KC_D,                        KC_H,     HOME_CMK_N,    HOME_CMK_E,    HOME_CMK_I,    HOME_CMK_O, LCTL_T(KC_QUOT),
-  KC_LSFT,  KC_Z,         KC_X,          KC_C,          KC_V,          KC_B,   KC_MUTE,    XXXXXXX, KC_K,     KC_M,          KC_COMM,       KC_DOT,        KC_SLSH,    KC_RSFT,
-                          KC_GRV,        KC_LALT,       KC_EQL,      KC_LOWER, KC_SPC,     KC_ENT,  KC_RAISE, KC_BSPC,       KC_RALT,       KC_RGUI
+  KC_ESC,   KC_1,         KC_2,          KC_3,          KC_4,          KC_5,                                KC_6,     KC_7,          KC_8,          KC_9,          KC_0,       KC_BSLS,
+  KC_TAB,   KC_Q,         KC_W,          KC_F,          KC_P,          KC_B,                                KC_J,     KC_L,          KC_U,          KC_Y,          KC_SCLN,    KC_BSPC,
+  KC_LCTL,  HOME_CMK_A,   HOME_CMK_R,    HOME_CMK_S,    HOME_CMK_T,    KC_G,                                KC_M,     HOME_CMK_N,    HOME_CMK_E,    HOME_CMK_I,    HOME_CMK_O, LCTL_T(KC_QUOT),
+  KC_LSFT,  KC_Z,         KC_X,          KC_C,          KC_D,          KC_V,   KC_MUTE,    XXXXXXX,         KC_K,     KC_H,          KC_COMM,       KC_DOT,        KC_SLSH,    KC_RSFT,
+                          KC_GRV,        KC_LALT,       KC_EQL,      KC_LOWER, KC_SPC,     LALT_T(KC_ENT),  KC_RAISE, KC_MINS,       KC_RALT,       KC_RGUI
 ),
 /* LOWER
  * cf BuJo
