fdd6e4d7e27efec156a90cd9f6434a00a1d6f4f3
Gerry Agbobada <git@gagbo.net>
2022-04-25 00:08:58 +0200

    [Sofle] Make use of the thumb keys

--- keyboards/sofle/keymaps/gagbo/keymap.c
+++ keyboards/sofle/keymaps/gagbo/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Mute  |    | Play  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   N  |   H  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LOWER| Space| / -     /       \ =    \  | Bspc | RAISE| RAlt | Ent  |
+ *            | LGUI | LAlt | LOWER| Space| / LSft  /       \ Ent  \  | Bspc | RAISE| RAlt | Ent  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q, KC_W,    KC_F,    KC_P,       KC_B,                          KC_J,    KC_L,       KC_U,    KC_Y,   KC_SCLN,    KC_BSLS,
   LCTL_T(KC_ESC), KC_A, KC_R,    KC_S,    KC_T,       KC_G,                          KC_M,    KC_N,       KC_E,    KC_I,   KC_O, RCTL_T(KC_QUOT),
   KC_LSFT,        KC_Z, KC_X,    KC_C,    KC_D,       KC_V,   KC_MUTE, /**/ KC_MPLY, KC_K,    KC_H,       KC_COMM, KC_DOT, KC_SLSH,    KC_RSFT,
-                        KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_MINS, /**/ KC_EQL,  KC_BSPC, MO(_RAISE), KC_RALT, KC_ENT
+                        KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_LSFT, /**/ KC_ENT,  KC_BSPC, MO(_RAISE), KC_RALT, KC_ENT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
