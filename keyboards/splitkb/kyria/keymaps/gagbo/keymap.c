c5edb95f47e0f1b87ac113d7f1fe9d94655545d2
Gerry Agbobada <git@gagbo.net>
2022-05-08 23:39:06 +0200

    [Kyria] Test new keymap

--- keyboards/splitkb/kyria/keymaps/gagbo/keymap.c
+++ keyboards/splitkb/kyria/keymaps/gagbo/keymap.c
@@ -23,8 +23,8 @@ enum layers {
     _SYM,
     _NUM,
     _FUN,
-    _MEDIA,
-    _MOUSE,
+    _MEDIADJUST,
+    _PADFUN,
 };
 
 #define QWERTY   DF(_QWERTY)
@@ -53,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_COLEMAK] = LAYOUT_kyria(
   KC_TAB,          KC_Q,         KC_W,          KC_F,          KC_P,          KC_B,                                                   KC_J,     KC_L,          KC_U,          KC_Y,           KC_SCLN,    KC_BSLS,
-  LCTL_T(KC_ESC),  HOME_CMK_A,   HOME_CMK_R,    HOME_CMK_S,    HOME_CMK_T,    KC_G,                                                   KC_M, HOME_CMK_N,        HOME_CMK_E,    HOME_CMK_I,     HOME_CMK_O, RCTL_T(KC_QUOT),
-  KC_LSFT,         KC_Z,         RALT_T(KC_X),  KC_C,          KC_D,          KC_V, KC_GRV, KC_MUTE,          /**/ KC_MPLY, KC_BSPC,  KC_K,     KC_H,          KC_COMM,       RALT_T(KC_DOT), KC_SLSH,    KC_RSFT,
-                                 KC_LGUI, KC_LALT, LT(_MEDIA, KC_ESC), LT(_NAV, KC_SPC), LT(_MOUSE, KC_TAB), /**/ LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), LT(_FUN, KC_DEL), KC_RALT, KC_RGUI
+  LCTL_T(KC_ESC),  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                                                   KC_M,      KC_N,        KC_E,    KC_I,     KC_O, RCTL_T(KC_QUOT),
+  KC_DEL,         KC_Z,         KC_X,  KC_C,          KC_D,          KC_V, KC_LALT, TG(_MEDIADJUST),          /**/ TG(_PADFUN), KC_LALT,  KC_K,     KC_H,          KC_COMM,       KC_DOT, KC_SLSH,    KC_RSFT,
+                                 KC_GRV, KC_LALT, MO(_NAV), KC_SPC, KC_LSFT, /**/ KC_ENT, KC_BSPC, MO(_SYM), KC_RALT, KC_RGUI
     ),
 
 /*
@@ -75,40 +75,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_kyria(
   KC_TAB,           KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_LCTL,          KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV, KC_MUTE,     KC_MPLY, KC_PSCR, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                                 KC_LGUI, KC_LALT, LT(_MEDIA, KC_ESC), LT(_NAV, KC_SPC), LT(_MOUSE, KC_TAB), /**/ LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), LT(_FUN, KC_DEL), KC_RALT, KC_RGUI
+  KC_LSFT,          KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV, KC_MUTE,     COLEMAK, KC_PSCR, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                                 KC_LGUI, KC_LALT, LT(_MEDIADJUST, KC_ESC), LT(_NAV, KC_SPC), LT(_PADFUN, KC_TAB), /**/ LT(_SYM, KC_ENT), LT(_NUM, KC_BSPC), LT(_FUN, KC_DEL), KC_RALT, KC_RGUI
     ),
 
 [_NAV] = LAYOUT_kyria(
-  KC_LALT, _______, _______, _______, _______, _______,                                            KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  KC_INS, _______,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                            KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_CAPS, _______,
-  _______, _______, KC_RALT, _______, _______, _______, _______, _______,        _______, _______, KC_AGIN, KC_PASTE, KC_COPY, KC_CUT,  KC_UNDO, _______,
-                             _______, _______, _______, _______, _______,        KC_ENT, KC_BSPC, KC_DEL, _______, _______
-),
-[_MOUSE] = LAYOUT_kyria(
-  KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                           KC_WH_L, KC_WH_D, KC_WH_U,  KC_WH_R, XXXXXXX, XXXXXXX,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                           KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,       _______, _______, KC_AGIN, KC_PASTE, KC_COPY, KC_CUT,  KC_UNDO, XXXXXXX,
-                             _______, _______, _______, _______, _______,       KC_BTN1, KC_BTN3, KC_BTN2, _______, _______
-),
-[_MEDIA] = LAYOUT_kyria(
-  KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                           KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,       _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                             _______, _______, _______, _______, _______,       KC_MSTP, KC_MPLY, KC_MUTE, _______, KC_PSCR
-),
-[_NUM] = LAYOUT_kyria(
-  KC_LALT, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_LCTL, KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                                        XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, XXXXXXX,
-  _______, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, _______, _______,   _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
-                             _______, _______, KC_DOT,  KC_0,    KC_MINS,   _______, _______, _______, _______, _______
+  _______, XXXXXXX, KC_LABK, KC_EQL, KC_RABK, XXXXXXX,                                             KC_AT, KC_PLUS,  KC_MINS, KC_SLSH,  KC_ASTR, _______,
+  _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_EXLM,                                            KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, KC_COLN, _______,
+  _______, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,        _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_CAPS, _______,
+                             _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______
 ),
 
 [_SYM] = LAYOUT_kyria(
-  KC_LALT, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_LCTL, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                                           XXXXXXX, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, XXXXXXX,
-  _______, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, _______, _______,       _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
-                             _______, _______, KC_LPRN, KC_RPRN, KC_UNDS,       _______, _______, _______, _______, _______
+  _______, KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_HASH,                                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, KC_QUES, KC_PERC, KC_LPRN, KC_RPRN, KC_UNDS,                                           KC_PIPE, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, KC_DQUO,
+  _______, KC_QUOT, KC_DLR,  KC_LCBR, KC_RCBR, KC_BSLS, _______, _______,       _______, _______, XXXXXXX, KC_TILD, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+
+[_NUM] = LAYOUT_kyria(
+  _______, KC_7,    KC_5,    KC_3,    KC_1,    KC_9,                                        KC_8,   KC_0,    KC_2,    KC_4,    KC_6,    XXXXXXX,
+  _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_F11,                                      KC_F12, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, KC_DOT,
+  _______, KC_F7,   KC_F5,   KC_F3,   KC_F1,   KC_F9, _______, _______,   _______, _______, KC_F8,  KC_F10,  KC_F2,   KC_F4,   KC_F6,   XXXXXXX,
+                             _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
+),
+
+[_PADFUN] = LAYOUT_kyria(
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,                                           KC_NUM, KC_P7, KC_P8,  KC_P9, KC_PAST, KC_PSLS,
+  _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_SLCK,                                           KC_PDOT, KC_P4, KC_P5, KC_P6, KC_PPLS, KC_PMNS,
+  _______, XXXXXXX, KC_RALT, XXXXXXX, KC_APP,  KC_PAUS, _______, _______,       _______, _______, KC_P0, KC_P1, KC_P2, KC_P3,  KC_PEQL, XXXXXXX,
+                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
+),
+[_MEDIADJUST] = LAYOUT_kyria(
+  XXXXXXX, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD,  XXXXXXX,                                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, RGB_RMOD, RGB_TOG,                                           KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, COLEMAK, QWERTY,   XXXXXXX, _______, _______,       _______, _______, KC_MSTP, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 
 [_FUN] = LAYOUT_kyria(
@@ -139,6 +141,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                  _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
 //     ),
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _SYM, _NAV, _NUM);
+}
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -199,16 +205,16 @@ static void render_status(void) {
             oled_write_P(PSTR("Sym\n"), false);
             break;
         case _NUM:
-            oled_write_P(PSTR("Num\n"), false);
+            oled_write_P(PSTR("Num F*\n"), false);
             break;
         case _FUN:
             oled_write_P(PSTR("Fun Meta\n"), false);
             break;
-        case _MEDIA:
-            oled_write_P(PSTR("Media\n"), false);
+        case _MEDIADJUST:
+            oled_write_P(PSTR("Media RGB\n"), false);
             break;
-        case _MOUSE:
-            oled_write_P(PSTR("Mouse\n"), false);
+        case _PADFUN:
+            oled_write_P(PSTR("Numpad\n"), false);
             break;
         default:
             oled_write_P(PSTR("Undef\n"), false);
