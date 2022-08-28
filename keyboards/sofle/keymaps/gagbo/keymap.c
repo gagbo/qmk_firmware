568383b48f74a422f1a22664ef7e31b8c03c9e27
Gerry Agbobada <git@gagbo.net>
2022-08-28 21:24:56 +0200

    [Sofle] Nope on the thumbs
    Keep the shift for HRM though

--- keyboards/sofle/keymaps/gagbo/keymap.c
+++ keyboards/sofle/keymaps/gagbo/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,     KC_W,      KC_E,      KC_R,    KC_T,                         KC_Y,    KC_U,       KC_I,      KC_O,     KC_P,      KC_LBRC,
   LCTL_T(KC_ESC),   HOME_A,   HOME_S,    HOME_D,    HOME_F,  KC_G,                         KC_H,  HOME_J,       HOME_K,    HOME_L,   HOME_SCLN, RCTL_T(KC_QUOT),
   KC_LSFT,          KC_Z,     HOME_X,    KC_C,      KC_V,    KC_B,  KC_MUTE,      KC_MPLY, KC_N,    KC_M,       KC_COMM,   HOME_DOT, KC_SLSH,   KC_RSFT,
-                            KC_DEL, KC_EQL, MO(_LOWER), KC_BSPC, KC_DEL,   /****/    KC_ENT,  KC_SPC, MO(_RAISE), KC_RBRC, KC_BSLS
+                            KC_DEL, KC_EQL, MO(_LOWER), KC_SPC, KC_DEL,   /****/    KC_ENT,  KC_BSPC, MO(_RAISE), KC_RBRC, KC_BSLS
 ),
 /*
  * QWERTY
