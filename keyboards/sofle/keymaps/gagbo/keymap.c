53e994c4558a7140c673989720aed57f9bd147e1
Gerry Agbobada <gerry@gagbo.net>
2023-06-19 01:48:51 +0200

    fix(sofle): adjust parens on symbol layer

--- keyboards/sofle/keymaps/gagbo/keymap.c
+++ keyboards/sofle/keymaps/gagbo/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
     KC_LGUI,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_RGUI,
     KC_LALT, OP_1,    OP_LBRC, OP_RBRC,  OP_DLR,  OP_PERC,                       OP_DCIR, OP_AMPR, OP_ASTR, OP_QUOT, OP_0,   KC_LALT,
-    KC_LCTL, OP_LCBR, KC_LPRN, KC_RPRN, OP_RCBR,  OP_EQL,                        OP_PLUS, OP_HMIN, OP_LABK, OP_RABK, OP_DQUO, KC_RCTL,
+    KC_LCTL, OP_LCBR, OP_LPRN, OP_RPRN, OP_RCBR,  OP_EQL,                        OP_PLUS, OP_HMIN, OP_LABK, OP_RABK, OP_DQUO, KC_RCTL,
     KC_LSFT, OP_DTIL, OP_GRV,  OP_PIPE, OP_UNDS,  OP_SLSH, _______,     _______, OP_BSLS, OP_AT,   OP_HASH, OP_EXLM, OP_QUES,  KC_RSFT,
                     KC_F11, KC_F12, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
