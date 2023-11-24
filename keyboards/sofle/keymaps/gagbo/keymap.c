f8153d481e07e600ae11ebebb5a8efa56fd2b2f2
Gerry Agbobada <git@gagbo.net>
2023-11-24 23:46:28 +0100

    Switch layout again to follow ergol transition

--- keyboards/sofle/keymaps/gagbo/keymap.c
+++ keyboards/sofle/keymaps/gagbo/keymap.c
@@ -66,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT(
   KC_GRV,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  OS_RALT,
-  KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_LBRC,
+  KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
   KC_ESC,  HOME_A, HOME_S,  HOME_D,    KC_F,    KC_G,                       KC_H,    KC_J,  HOME_K,  HOME_L, HOME_SCLN, KC_QUOT,
-  KC_GRV,  KC_Z,   HOME_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,    KC_MPLY, KC_N,    KC_M, KC_COMM, HOME_DOT, KC_SLSH,  KC_RBRC,
-                         XXXXXXX, XXXXXXX, OS_SHFT , KC_SPC,  LWR  , /**/   RSE  , KC_BSPC, OS_SHFT, XXXXXXX, XXXXXXX
+  KC_GRV,  KC_Z,   HOME_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,    KC_MPLY, KC_N,    KC_M, KC_COMM, HOME_DOT, KC_SLSH,  KC_BSLS,
+                         XXXXXXX, XXXXXXX, OS_SHFT , KC_SPC,  LWR  , /**/   RSE  , KC_BSPC, KC_RSFT, XXXXXXX, XXXXXXX
 ),
 
 /*
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, OP_AGRV,  OP_J  ,  OP_O  , OP_EACU,  OP_B  ,                          OP_F  ,  OP_D  ,  OP_L  , OP_QUOT, OP_DCIR,  OP_X  ,
   KC_ESC,  HOME_A, HOME_OI, HOME_OE,  OP_U  , OP_COMM,                          OP_P  ,  OP_T  , HOME_OS, HOME_OR, HOME_ON, OP_EGRV,
   OP_DLR,  OP_K  , HOME_OY,  OP_Q  ,  OP_DOT,  OP_W  , KC_MUTE,       KC_MPLY,  OP_G  ,  OP_C  ,  OP_M  ,  OP_H  ,  OP_V  ,  OP_Z  ,
-                         XXXXXXX, XXXXXXX, OS_SHFT, KC_SPC, LWR , /**/   RSE  , KC_BSPC, OS_SHFT, XXXXXXX, XXXXXXX
+                         XXXXXXX, XXXXXXX, OS_SHFT, KC_SPC, LWR , /**/   RSE  , KC_BSPC, KC_RSFT, XXXXXXX, XXXXXXX
 ),
 
 
@@ -98,16 +98,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-[_RAISE] = LAYOUT(
+[_LOWER] = LAYOUT(
   KC_F12,  KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11,
-  XXXXXXX, _______, XXXXXXX,  _______, _______,  KC_PSCR,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLU, KC_F11,
-  XXXXXXX, OS_GUI, OS_ALT,  OS_CTRL,  OS_SHFT,  CW_TOGG,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, XXXXXXX,
+  XXXXXXX, _______, XXXXXXX,  _______, _______, KC_CAPS,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLU, KC_PSCR,
+  XXXXXXX, OS_GUI, OS_ALT,  OS_CTRL,  OS_SHFT,  CW_TOGG,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_INS,
   _______, _______, OS_RALT,  XXXXXXX,  XXXXXXX,  XXXXXXX, _______,    _______, KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_F12,
-                    _______, _______, _______,  _______,   BOTH,    _______, _______, _______, _______, _______
+                    _______, _______, _______,  _______, _______,      BOTH , _______, _______, _______, _______
 ),
 
-/* Lower (Num layer)
- * Symboles Optimot
+/* Num layer
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | Esc  |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  | F9   | F10  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -121,12 +120,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-[_LOWER] = LAYOUT(
+[_RAISE] = LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
-    _______, OP_1,    OP_2   ,  OP_3  ,  OP_4  ,  OP_5  ,                        OP_6  ,  OP_7  ,  OP_8  ,  OP_9  ,  OP_0  , OP_HASH,
-    _______, H_LDAQ , H_RDAQ,   H_DQUO, OP_HMIN, OP_PLUS,                       OP_ASTR, OP_SLSH, OP_EQL , OP_LPRN, OP_RPRN, XXXXXXX,
-    OP_DLR , XXXXXXX, OS_RALT, XXXXXXX, OP_DLR , OP_EURO, _______,     _______, XXXXXXX, OP_UNDS, OP_PERC, OS_RALT, XXXXXXX,  OP_AT ,
-                    _______, _______, _______, _______, _______,       BOTH, _______, _______, _______, _______
+    _______, KC_LBRC,   KC_7 ,   KC_8 ,   KC_9 , KC_RBRC,                       KC_ASTR, KC_PLUS, KC_MINS, KC_SLSH, XXXXXXX, XXXXXXX,
+    _______, KC_SCLN,   KC_4 ,   KC_5 ,   KC_6 ,  KC_EQL,                       XXXXXXX, OS_SHFT, OS_RALT, OS_ALT , OS_GUI , XXXXXXX,
+    KC_GRV , KC_MINS,   KC_1 ,   KC_2 ,   KC_3 , KC_BSLS, _______,     _______, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
+                    _______, _______, _______, _______,   BOTH ,       _______, _______, _______, _______, _______
 ),
 
 /* Function
@@ -144,10 +143,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_BOTH] = LAYOUT(
-  QK_BOOT,  XXXXXXX, XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_QWERTY, KC_OPTIMOT, XXXXXXX, CG_TOGG,   KC_PSCR,                      XXXXXXX,  KC_F7 ,  KC_F8 ,  KC_F9 ,  KC_F12, XXXXXXX,
-  XXXXXXX,  OS_GUI ,  OS_ALT ,  OS_CTRL,   OS_SHFT,   XXXXXXX,                      XXXXXXX,  KC_F4 ,  KC_F5 ,  KC_F6 ,  KC_F11, XXXXXXX,
-  _______, XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX, _______,    _______, XXXXXXX,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F10, _______,
+  QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_F12 ,  KC_F7 ,  KC_F8 ,  KC_F9 , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QWERTY,
+  XXXXXXX, KC_F11 ,  KC_F4 ,  KC_F5 ,  KC_F6 , XXXXXXX,                      CG_TOGG, OS_SHFT, OS_CTRL, OS_ALT , OS_GUI , KC_OPTIMOT,
+  _______, KC_F10 ,  KC_F1 ,  KC_F2 ,  KC_F3 , XXXXXXX, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                 _______, _______, _______, _______,  _______,     _______, _______, _______, _______, _______
 )
 
@@ -195,10 +194,10 @@ static void print_status_narrow(void) {
             oled_write_P(PSTR("Qwrt\n"), false);
             break;
         case _LOWER:
-            oled_write_P(PSTR("Num\n"), false);
+            oled_write_P(PSTR("Nav\n"), false);
             break;
         case _RAISE:
-            oled_write_P(PSTR("Nav\n"), false);
+            oled_write_P(PSTR("Num\n"), false);
             break;
         case _BOTH:
             oled_write_P(PSTR("Fun\n"), false);
